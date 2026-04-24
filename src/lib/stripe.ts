6a2c7badbcaf3e9c0a4107a6b3a1c72226d412cb
Sagar Moriya <sagarmaurya814@gmail.com>
2026-04-24 19:59:22 +0530

    fixed stripe build error

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,13 +1,13 @@
 import Stripe from "stripe";
 
 if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error("Missing STRIPE_SECRET_KEY");
+  throw new Error("Missing STRIPE_SECRET_KEY");
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2025-02-24.acacia", // specify latest API version or your chosen one
-    appInfo: {
-        name: "Golf Draw & Charity App",
-        version: "0.1.0"
-    }
+  apiVersion: "2026-03-25.dahlia", // specify latest API version or your chosen one
+  appInfo: {
+    name: "Golf Draw & Charity App",
+    version: "0.1.0",
+  },
 });
