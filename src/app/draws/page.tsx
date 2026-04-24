4bd17e2502cca6d117151701b892f40adb1de302
Sagar Moriya <sagarmaurya814@gmail.com>
2026-04-24 20:23:05 +0530

    fixed build error

--- src/app/draws/page.tsx
+++ src/app/draws/page.tsx
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function DrawsPage() {
     const draws = await prisma.draw.findMany({
         where: { isPublished: true },
